fc8a8168614a7b5a000e2c9f6dfabb2bd189ea83
Tim Cuddeback <cuddebtj@gmail.com>
2023-11-21 04:27:58 +0000

    dev

--- src/prefect_orchestration/modules/blocks.py
+++ src/prefect_orchestration/modules/blocks.py
@@ -10,30 +10,24 @@ def notify_discord_failure(flow: Flow, flow_run: FlowRun, state: State) -> None:
     body = """
 --------------------------------------------------------------------
 # FAILED JOB:
-
 ## `{flow_name}`
-
 ### Tags:
 - {flow_tags}
 ### INFO:
-
 - **Scheduled start:**
     - {flow_start_time}
 - **Total run time:**
     - {total_run_time}
 - **Access URL:**
-    - https://{prefect_url}/flow-run/{flow_id}
+    - https://{prefect_url}/flow-runs/flow-run/{flow_id}
 - **State Name:**
     - {state_name}
-- **State Data:**
-    - {state_data}
 - **State Message:**
     - {state_message}
 - **Deployment ID:**
     - {deployment_id}
 - **Parent Task ID:**
     - {parent_task_run_id}
-
 ### Parameters:
 - {flow_parameters}
 """
@@ -72,32 +66,38 @@ def notify_discord_cancellation(flow: Flow, flow_run: FlowRun, state: State) -> 
     body = """
 --------------------------------------------------------------------
 # CANCELLED JOB:
-
 ## `{flow_name}`
-
 ### Tags:
 - {flow_tags}
 ### INFO:
-
 - **Scheduled start:**
     - {flow_start_time}
 - **Total run time:**
     - {total_run_time}
 - **Access URL:**
-    - https://{prefect_url}/flow-run/
-    - {flow_id}
+    - https://{prefect_url}/flow-runs/flow-run/{flow_id}
 - **State Name:**
     - {state_name}
-- **State Data:**
-    - {state_data}
+- **State Message:**
+    - {state_message}
 - **Deployment ID:**
     - {deployment_id}
 - **Parent Task ID:**
     - {parent_task_run_id}
-
 ### Parameters:
 - {flow_parameters}
 """
+    conceal_list = [
+        "db_conn_uri",
+        "consumer_secret",
+        "consumer_key",
+        "tokey_file_path",
+        "yahoo_consumer_key",
+        "yahoo_consumer_secret",
+    ]
+    parameters = [
+        f"{k}: `{v}`\n" if k not in conceal_list else f"{k}: `{SecretStr(v)}`\n" for k, v in flow_run.parameters.items()
+    ]
     discord_webhook_block.notify(  # type: ignore
         body.format(
             flow_name=flow.name,
@@ -109,9 +109,10 @@ def notify_discord_cancellation(flow: Flow, flow_run: FlowRun, state: State) -> 
             state_name=state.name,
             state_type=state.type,
             state_data=state.data,
+            state_message=state.message,
             deployment_id=flow_run.deployment_id,
             parent_task_run_id=flow_run.parent_task_run_id,
-            flow_parameters="- ".join(f"{k}: `{v}`\n" for k, v in flow_run.parameters.items()),
+            flow_parameters="- ".join(parameters),
         )
     )
 
